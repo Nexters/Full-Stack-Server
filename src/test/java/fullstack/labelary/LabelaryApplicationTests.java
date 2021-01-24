c0dc342f3b9355f1e0065e70a2f0f846747f2fcb
Young-Jun Park <youngjun108059@gmail.com>
2021-01-24 18:24:08 +0900

    [Init]🎉Spring boot project - Labelary #1

---
+++ src/test/java/fullstack/labelary/LabelaryApplicationTests.java
@@ -0,0 +1,13 @@
+package fullstack.labelary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabelaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
