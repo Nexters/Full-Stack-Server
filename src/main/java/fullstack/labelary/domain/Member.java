8cb3b079328e0d624abd67acd1193aaadcbd949b
YoungJun Park <youngjun108059@gmail.com>
2021-02-11 15:17:06 +0900

    [#2] member 도메인 수정

--- src/main/java/fullstack/labelary/domain/Member.java
+++ src/main/java/fullstack/labelary/domain/Member.java
@@ -3,16 +3,22 @@ package fullstack.labelary.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
 public class Member {
 
     @Id @GeneratedValue
-    private Long memIdx;        // 회원 idx
-    private String memName;     // 회원 이름
+    @Column(name = "mem_idx")
+    private Long memIdx;            // 회원 idx
+    private String memName;         // 회원 이름
+    private String deviceIdx;       // 스마트폰 idx
+    private LocalDateTime memRegDt; // 회원가입 일짜
 
+    @OneToMany(mappedBy = "member")
+    private List<Relation> relation = new ArrayList<>();
 }
