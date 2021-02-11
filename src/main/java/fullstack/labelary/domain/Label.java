545475335894f5b86acb8c245f9f24a600d0014b
YoungJun Park <youngjun108059@gmail.com>
2021-02-11 15:17:29 +0900

    [#2] 라벨 도메인 수정

--- src/main/java/fullstack/labelary/domain/Label.java
+++ src/main/java/fullstack/labelary/domain/Label.java
@@ -3,20 +3,26 @@ package fullstack.labelary.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
 public class Label {
 
     @Id @GeneratedValue
-    private int labelIdx;       // 라벨 번호
-    private String labelTitle;  // 라벨 타이틀
-    private String labelDetail; // 라벨 설명
-    private String labelCount;  // 라벨 사용 횟수
-    private String labelDt;     // 라벨 생성 시간
-    private String labelColor;  // 라벨 색
+    @Column(name = "label_idx")
+    private Long labelIdx;          // 라벨 번호
+    private String labelTitle;      // 라벨 타이틀
+    private String labelDetail;     // 라벨 설명
+    private int labelCount;         // 라벨 사용 횟수
+    private LocalDateTime labelDt;  // 라벨 생성 시간
+    private String labelColor;      // 라벨 색
+    private LocalDateTime searchDt; // 검색 시간
+
+    @OneToMany(mappedBy = "label")
+    private List<Relation> relation = new ArrayList<>();
 
 }
