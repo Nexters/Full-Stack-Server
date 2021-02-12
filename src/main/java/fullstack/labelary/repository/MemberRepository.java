926e1177d8c0dacb63003c0a067442e1c73584fd
YoungJun Park <youngjun108059@gmail.com>
2021-02-12 23:10:04 +0900

    [#6] 회원 조회 기능 추가(JPA)

--- src/main/java/fullstack/labelary/repository/MemberRepository.java
+++ src/main/java/fullstack/labelary/repository/MemberRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public class MemberRepository {
@@ -13,7 +14,7 @@ public class MemberRepository {
     private EntityManager em;
 
     /**
-     * JPA - 생성
+     * 회원 생성
      *
      * @param member 저장될 정보
      * @return 생성된 Member IDX
@@ -24,12 +25,35 @@ public class MemberRepository {
     }
 
     /**
-     * JPA - 조회
+     * 회원 idx 통한 조회
      *
      * @param id 조회할 member idx
      * @return 일치하는 Member 정보
      */
-    public Member find(Long id) {
+    public Member findOne(Long id) {
         return em.find(Member.class, id);
     }
+
+    /**
+     * 모든 회원 조회
+     *
+     * @return Member List
+     */
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    /**
+     * 이름으로 회원 조회
+     *
+     * @param name 회원 이름
+     * @return Member List
+     */
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.memName = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+
 }
