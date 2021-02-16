c76deba4eae5d8ef71c92aa038d834d4604cb6f6
YoungJun Park <youngjun108059@gmail.com>
2021-02-16 23:42:26 +0900

    [#9] 사진 도메인 수정

--- src/main/java/fullstack/labelary/domain/Picture.java
+++ src/main/java/fullstack/labelary/domain/Picture.java
@@ -15,11 +15,11 @@ public class Picture {
     @Id @GeneratedValue
     @Column(name = "picture_idx")
     private Long pictureIdx;            // 사진 번호
-    private int pictureCount;           // 사진 본 횟수
     private LocalDateTime pictureDt;    // 사진 생성 시간
     private LocalDateTime lastSearched; // 마지막 조회 시간
     private String thumbnail;           // 썸네일 URL
     private String originUrl;           // 원본 URL
+    private Boolean bookmark;           // 즐겨찾기 유무
 
     @OneToMany(mappedBy = "picture")
     private List<Relation> relation = new ArrayList<>();
