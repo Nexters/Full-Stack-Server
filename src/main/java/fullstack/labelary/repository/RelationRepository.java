fc477bcbf17a45e63b854ff7a239cc568e381a8c
YoungJun Park <youngjun108059@gmail.com>
2021-02-22 18:25:57 +0900

    [#11] 라벨링 조회 & 삭제 JPA 추가

--- src/main/java/fullstack/labelary/repository/RelationRepository.java
+++ src/main/java/fullstack/labelary/repository/RelationRepository.java
@@ -1,6 +1,5 @@
 package fullstack.labelary.repository;
 
-import fullstack.labelary.domain.Picture;
 import fullstack.labelary.domain.Relation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -31,34 +30,64 @@ public class RelationRepository {
     }
 
     /**
-     * 사진 조회 idx 매칭 조회하기
+     * 라벨링 조회
      *
-     * @param idx 조회할 사진 id
+     * @param idx 조회할 라벨링 정보
      * @return 사진 Object
      */
-    public Picture findOne(Long idx) {
-        return em.find(Picture.class, idx);
+    public Relation findOne(Long idx) {
+        return em.find(Relation.class, idx);
     }
 
     /**
-     * 모든 사진 조회
+     * 모든 라벨링 관계 조회
      *
      * @return 사진 List
      */
-    public List<Picture> findAll() {
-        return em.createQuery("select p from Picture p", Picture.class)
+    public List<Relation> findAll() {
+        return em.createQuery("select r from Relation r", Relation.class)
                 .getResultList();
     }
 
     /**
-     * idx 일치 사진 삭제
+     * 현재 회원의 모든 라벨링 조회
      *
-     * @param idx 삭제할 사진 idx
+     * @param idx 조회할 member idx
+     * @return 라벨링 관계 list
      */
-    public void delete(Long idx) {
-        Picture toDeletePicture = em.find(Picture.class, idx);
-        em.remove(toDeletePicture);
+    public List<Relation> findByMember(Long idx) {
+        return em.createQuery("select r from Relation r where r.memIdx = :memIdx", Relation.class)
+                .setParameter("memIdx", idx)
+                .getResultList();
     }
 
-    // 사진 즐겨찾기
+
+    /**
+     * 회원+사진 idx 일치하는 라벨 리스트
+     *
+     * @param memIdx 회원 번호
+     * @param pictureIdx 사진 번호
+     * @return 일치하는 라벨링 리스트
+     */
+    public List<Relation> findLabelList(Long memIdx, Long pictureIdx) {
+        return em.createQuery("select r from Relation r " +
+                "where r.memIdx = :memIdx and r.pictureIdx = :pictureIdx", Relation.class)
+                .setParameter("memIdx", memIdx)
+                .setParameter("pictureIdx", pictureIdx)
+                .getResultList();
+    }
+
+
+    /**
+     * idx 일치 라벨링 삭제
+     * // Todo 사진 삭제 시 관련 라벨링 row 전부 삭제 (사진 기능)
+     * // Todo 라벨 삭제 시 관련 라벨링 row 전부 삭제 (앨범 기능)
+     *
+     * @param idx 삭제할 라벨링 관계 idx
+     */
+    public void delete(Long idx) {
+        Relation toDeleteRelation = em.find(Relation.class, idx);
+        em.remove(toDeleteRelation);
+    }
+
 }
