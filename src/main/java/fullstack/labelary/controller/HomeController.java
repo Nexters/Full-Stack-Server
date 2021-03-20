555c3f8837be40ea7f97208135734a1d31897da4
YoungJun Park <youngjun108059@gmail.com>
2021-03-20 17:56:38 +0900

    [#7] Dto를 활용한 컨트롤러 RequestParam 동작시키기

--- src/main/java/fullstack/labelary/controller/HomeController.java
+++ src/main/java/fullstack/labelary/controller/HomeController.java
@@ -1,8 +1,10 @@
 package fullstack.labelary.controller;
 
+import fullstack.labelary.service.dto.HelloResponseDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,6 +16,14 @@ public class HomeController {
     public String home() {
         log.info("home controller");
         return "home";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                           @RequestParam("amount") int amount) {
+        log.info("API RequestParam name : {}, amount : {}", name, amount);
+
+        return new HelloResponseDto(name, amount);
     }
 
 }
--- src/test/java/fullstack/labelary/controller/HomeControllerTest.java
+++ src/test/java/fullstack/labelary/controller/HomeControllerTest.java
@@ -7,9 +7,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 
 @RunWith(SpringRunner.class)
@@ -28,5 +30,22 @@ public class HomeControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("home"));
+    }
+
+    @Test
+    public void returnHelloDtoCheck() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        // jsonPath : JSON 응답값을 필드별로 검증할 수 있음
+        // $를 기준으로 필드명을 명시
+        // param : API 테스트할 때 사용될 요청 파라미터 설정
+        // string 만 허용!
+        mvc.perform(get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
