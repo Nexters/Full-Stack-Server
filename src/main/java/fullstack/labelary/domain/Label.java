107be0cd303ecc73a4773f78f7b9d8e2749e8de9
YoungJun Park <youngjun108059@gmail.com>
2021-05-16 23:03:40 +0900

    [#8] 라벨 도메인 수정

--- src/main/java/fullstack/labelary/domain/Label.java
+++ src/main/java/fullstack/labelary/domain/Label.java
@@ -1,28 +1,44 @@
 package fullstack.labelary.domain;
 
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
+@Getter
+@NoArgsConstructor
 @Entity
-@Getter @Setter
-public class Label {
+public class Label extends BaseTimeEntity {
 
-    @Id @GeneratedValue
-    @Column(name = "label_idx")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long labelIdx;          // 라벨 번호
+
     private String labelTitle;      // 라벨 타이틀
+
     private String labelDetail;     // 라벨 설명
+
     private int labelCount;         // 라벨 사용 횟수
-    private LocalDateTime labelDt;  // 라벨 생성 시간
+
     private String labelColor;      // 라벨 색
+
     private LocalDateTime searchDt; // 검색 시간
 
-    @OneToMany(mappedBy = "label")
-    private List<Relation> relation = new ArrayList<>();
+    @Builder
+    public Label(String labelTitle, String labelDetail, String labelColor) {
+        this.labelTitle = labelTitle;
+        this.labelDetail = labelDetail;
+        this.labelColor = labelColor;
+    }
 
+    public void update(String labelTitle, String labelDetail, String labelColor) {
+        this.labelTitle = labelTitle;
+        this.labelDetail = labelDetail;
+        this.labelColor = labelColor;
+    }
 }
