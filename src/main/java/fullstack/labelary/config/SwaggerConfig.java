01c6c9535db90a93043a49f1fedcb039d12a2c10
YoungJun Park <youngjun108059@gmail.com>
2021-05-20 22:19:48 +0900

    [#3] Swagger Config 추가

---
+++ src/main/java/fullstack/labelary/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package fullstack.labelary.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Labelary API Docs")
+                        .version("v1.0.0")
+                        .description("About Labelary APIs"));
+    }
+
+}
