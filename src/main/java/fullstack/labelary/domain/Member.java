3afa1356ccfc7ef425c5ab29b8899387ac815d87
YoungJun Park <youngjun108059@gmail.com>
2021-05-23 00:21:51 +0900

    [Feature] Security 기반 Member 구성

--- src/main/java/fullstack/labelary/domain/Member.java
+++ src/main/java/fullstack/labelary/domain/Member.java
@@ -1,23 +1,49 @@
 package fullstack.labelary.domain;
 
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import java.time.LocalDateTime;
+import javax.persistence.*;
 
+@Getter
+@NoArgsConstructor
 @Entity
-@Getter @Setter
-public class Member {
+public class Member extends BaseTimeEntity {
 
-    @Id @GeneratedValue
-    private Long memIdx;            // 회원 idx
-    private String memEmail;        // 이메일
-    private String memPassword;     // 비밀번호
-    private String memName;         // 회원 이름
-    private String deviceIdx;       // 스마트폰 idx
-    private LocalDateTime memRegDt; // 회원가입 일짜
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memIdx; // 회원 idx
 
+    @Column(nullable = false)
+    private String memEmail; // 이메일
+
+    @Column(nullable = false)
+    private String memName; // 회원 이름
+
+    @Column(nullable = false)
+    private String picture; // 회원 프로필사진
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role; // 스프링 시큐리티를 위한 권한 코드
+
+    @Builder
+    public Member(String memEmail, String memName, String picture, Role role) {
+        this.memEmail = memEmail;
+        this.memName = memName;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public Member update(String memName, String picture) {
+        this.memName = memName;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
 }
--- src/main/java/fullstack/labelary/repository/MemberRepository.java
+++ src/main/java/fullstack/labelary/repository/MemberRepository.java
@@ -1,82 +1,10 @@
 package fullstack.labelary.repository;
 
 import fullstack.labelary.domain.Member;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.util.List;
+import java.util.Optional;
 
-@Repository
-public class MemberRepository {
-
-    @PersistenceContext
-    private EntityManager em;
-
-    /**
-     * 회원 생성
-     *
-     * @param member 저장될 정보
-     * @return 생성된 Member IDX
-     */
-    public Long save(Member member) {
-        em.persist(member);
-        return member.getMemIdx();
-    }
-
-    /**
-     * 회원 idx 통한 조회
-     *
-     * @param id 조회할 member idx
-     * @return 일치하는 Member 정보
-     */
-    public Member findOne(Long id) {
-        return em.find(Member.class, id);
-    }
-
-    /**
-     * 모든 회원 조회
-     *
-     * @return Member List
-     */
-    public List<Member> findAll() {
-        return em.createQuery("select m from Member m", Member.class)
-                .getResultList();
-    }
-
-    /**
-     * 이름으로 회원 조회
-     *
-     * @param name 회원 이름
-     * @return Member List
-     */
-    public List<Member> findByName(String name) {
-        return em.createQuery("select m from Member m where m.memName = :name", Member.class)
-                .setParameter("name", name)
-                .getResultList();
-    }
-
-    /**
-     * 이메일로 회원 조회
-     *
-     * @param email 회원 이메일
-     * @return Member List
-     */
-    public List<Member> findByEmail(String email) {
-        return em.createQuery("select m from Member m where m.memEmail = :email", Member.class)
-                .setParameter("email", email)
-                .getResultList();
-    }
-
-    /**
-     * 회원 삭제
-     *
-     * @param memIdx 회원 idx
-     */
-    public void deleteMember(Long memIdx) {
-        Member member = em.find(Member.class, memIdx);
-        em.remove(member);
-    }
-
-
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByMemEmail(String email);
 }
