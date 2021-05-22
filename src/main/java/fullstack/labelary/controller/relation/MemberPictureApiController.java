7e99d4bf1bfc11b1144688adcf209ea54c502fde
YoungJun Park <youngjun108059@gmail.com>
2021-05-22 22:31:20 +0900

    [Feature] 회원-사진 로직 비활성화

--- src/main/java/fullstack/labelary/controller/relation/MemberPictureApiController.java
+++ src/main/java/fullstack/labelary/controller/relation/MemberPictureApiController.java
@@ -1,36 +1,36 @@
-package fullstack.labelary.controller.relation;
-
-import fullstack.labelary.dto.ApiResponse;
-import fullstack.labelary.dto.relation.CreateMemberPictureRequest;
-import fullstack.labelary.dto.relation.MemberPictureResponse;
-import io.swagger.v3.oas.annotations.Operation;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-
-@Controller
-@RequiredArgsConstructor
-public class MemberPictureApiController {
-    private final MemberPictureService MemberPictureService;
-
-    @Operation(summary = "내 사진을 등록하는 API")
-    @PostMapping("/api/v1/member/picture")
-    public ApiResponse<MemberPictureResponse> addMemberPicture(@Valid @RequestBody CreateMemberPictureRequest request, @MemberId Long memberId) {
-        return ApiResponse.success(MemberPictureService.addMemberPicture(request, memberId));
-    }
-
-    @Operation(summary = "내 사진들을 조회하는 API")
-    @GetMapping("/api/v1/member/picture")
-    public ApiResponse<List<StockCalculateResponse>> getStocksInfo(@RequestParam StockMarketType type, @MemberId Long memberId) {
-        return ApiResponse.success(MemberPictureRetrieveService.getMemberCurrentStocks(type, memberId));
-    }
-
-    @Operation(summary = "내 사진을 삭제하는 API")
-    @DeleteMapping("/api/v1/member/picture")
-    public ApiResponse<String> deleteMemberPicture(@Valid DeleteMemberPictureRequest request, @MemberId Long memberId) {
-        MemberPictureService.deleteMemberPicture(request, memberId);
-        return ApiResponse.SUCCESS;
-    }
-}
+//package fullstack.labelary.controller.relation;
+//
+//import fullstack.labelary.dto.ApiResponse;
+//import fullstack.labelary.dto.relation.CreateMemberPictureRequest;
+//import fullstack.labelary.dto.relation.MemberPictureResponse;
+//import io.swagger.v3.oas.annotations.Operation;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.*;
+//
+//import javax.validation.Valid;
+//
+//@Controller
+//@RequiredArgsConstructor
+//public class MemberPictureApiController {
+//    private final MemberPictureService MemberPictureService;
+//
+//    @Operation(summary = "내 사진을 등록하는 API")
+//    @PostMapping("/api/v1/member/picture")
+//    public ApiResponse<MemberPictureResponse> addMemberPicture(@Valid @RequestBody CreateMemberPictureRequest request, @MemberId Long memberId) {
+//        return ApiResponse.success(MemberPictureService.addMemberPicture(request, memberId));
+//    }
+//
+//    @Operation(summary = "내 사진들을 조회하는 API")
+//    @GetMapping("/api/v1/member/picture")
+//    public ApiResponse<List<StockCalculateResponse>> getStocksInfo(@RequestParam StockMarketType type, @MemberId Long memberId) {
+//        return ApiResponse.success(MemberPictureRetrieveService.getMemberCurrentStocks(type, memberId));
+//    }
+//
+//    @Operation(summary = "내 사진을 삭제하는 API")
+//    @DeleteMapping("/api/v1/member/picture")
+//    public ApiResponse<String> deleteMemberPicture(@Valid DeleteMemberPictureRequest request, @MemberId Long memberId) {
+//        MemberPictureService.deleteMemberPicture(request, memberId);
+//        return ApiResponse.SUCCESS;
+//    }
+//}
--- src/main/java/fullstack/labelary/service/realtion/MemberPictureService.java
+++ src/main/java/fullstack/labelary/service/realtion/MemberPictureService.java
@@ -1,70 +1,70 @@
-package fullstack.labelary.service.realtion;
-
-
-import fullstack.labelary.domain.Member;
-import fullstack.labelary.repository.relation.MemberPictureRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-
-@RequiredArgsConstructor
-@Transactional(readOnly = true)
-@Service
-public class MemberPictureService {
-    private final MemberPictureRepository memberPictureRepository;
-
-    /**
-     * 회원-사진 관계 등록
-     *
-     * @param member 회원 정보
-     * @return 등록된 회원 idx
-     */
-    @Transactional
-    public Long join(Member member) {
-        return memberRepository.save(member);
-    }
-
-    /**
-     * 전체 회원 조회
-     *s
-     * @return member List
-     */
-    public List<Member> findAllMembers() {
-        return memberRepository.findAll();
-    }
-
-    /**
-     * 회원 Idx 값으로 회원 정보 1건 조회
-     *
-     * @param memberIdx 회원 Idx
-     * @return 회원 정보
-     */
-    public Member findOneMember(Long memberIdx) {
-        return memberRepository.findOne(memberIdx);
-    }
-
-    /**
-     * 회원 이름 업데이트
-     *
-     * @param memIdx  회원 idx
-     * @param memName 변경할 이름
-     */
-    @Transactional
-    public void update(Long memIdx, String memName) {
-        // 변경 감지 데이터 수정
-        Member member = memberRepository.findOne(memIdx);
-        member.setMemName(memName);
-    }
-
-    /**
-     * 회원 삭제
-     *
-     * @param memIdx 삭제할 회원 정보
-     */
-    public void deleteMember(Long memIdx) {
-        memberRepository.deleteMember(memIdx);
-    }
-
-}
+//package fullstack.labelary.service.realtion;
+//
+//
+//import fullstack.labelary.domain.Member;
+//import fullstack.labelary.repository.relation.MemberPictureRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.util.List;
+//
+//@RequiredArgsConstructor
+//@Transactional(readOnly = true)
+//@Service
+//public class MemberPictureService {
+//    private final MemberPictureRepository memberPictureRepository;
+//
+//    /**
+//     * 회원-사진 관계 등록
+//     *
+//     * @param member 회원 정보
+//     * @return 등록된 회원 idx
+//     */
+//    @Transactional
+//    public Long join(Member member) {
+//        return memberRepository.save(member);
+//    }
+//
+//    /**
+//     * 전체 회원 조회
+//     *s
+//     * @return member List
+//     */
+//    public List<Member> findAllMembers() {
+//        return memberRepository.findAll();
+//    }
+//
+//    /**
+//     * 회원 Idx 값으로 회원 정보 1건 조회
+//     *
+//     * @param memberIdx 회원 Idx
+//     * @return 회원 정보
+//     */
+//    public Member findOneMember(Long memberIdx) {
+//        return memberRepository.findOne(memberIdx);
+//    }
+//
+//    /**
+//     * 회원 이름 업데이트
+//     *
+//     * @param memIdx  회원 idx
+//     * @param memName 변경할 이름
+//     */
+//    @Transactional
+//    public void update(Long memIdx, String memName) {
+//        // 변경 감지 데이터 수정
+//        Member member = memberRepository.findOne(memIdx);
+//        member.setMemName(memName);
+//    }
+//
+//    /**
+//     * 회원 삭제
+//     *
+//     * @param memIdx 삭제할 회원 정보
+//     */
+//    public void deleteMember(Long memIdx) {
+//        memberRepository.deleteMember(memIdx);
+//    }
+//
+//}
