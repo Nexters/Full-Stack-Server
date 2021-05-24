9a6b370a6c0f843f1ba9aef30e7fac6338c46c28
YoungJun Park <youngjun108059@gmail.com>
2021-05-25 01:52:27 +0900

    [#14] 라벨 API Test

--- src/main/java/fullstack/labelary/domain/Label.java
+++ src/main/java/fullstack/labelary/domain/Label.java
@@ -18,15 +18,10 @@ public class Label extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long labelIdx;          // 라벨 번호
-
     private String labelTitle;      // 라벨 타이틀
-
     private String labelDetail;     // 라벨 설명
-
     private int labelCount;         // 라벨 사용 횟수
-
     private String labelColor;      // 라벨 색
-
     private LocalDateTime searchDt; // 검색 시간
 
     @Builder
