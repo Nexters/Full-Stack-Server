6820404239c851e6a0caf0dad09bd7a27261eeda
YoungJun Park <youngjun108059@gmail.com>
2021-06-02 01:36:32 +0900

    [#10] 관리자 접근 권한 Config 수정

--- src/main/java/fullstack/labelary/config/auth/SecurityConfig.java
+++ src/main/java/fullstack/labelary/config/auth/SecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @RequiredArgsConstructor
-@EnableWebSecurity
+@EnableWebSecurity // Spring Security 설정 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CustomOAuth2UserService customOAuth2UserService;
@@ -15,20 +15,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-                .headers().frameOptions().disable()
-                .and()
                 .authorizeRequests()
-                .antMatchers("/", "/css/**", "/images/**",
-                        "/js/**", "/h2-console/**").permitAll()
-                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .antMatchers(
+                        // 누구나 접근 가능한 설정
+                        "/",
+                        "/css/**",
+                        "/images/**",
+                        "/js/**",
+                        "/h2-console/**",
+                        "/login/**"
+                ).permitAll()
+                .antMatchers(
+                        // "USER"인 경우만 접근 가능
+                        "/api/v1/**",
+                        "/admin/**"
+                ).hasRole(Role.USER.name())
+                // 나머지 URL 요청은 무조건 인증
                 .anyRequest().authenticated()
-                .and()
+            .and()
                 .logout()
-                .logoutSuccessUrl("/")
-                .and()
+                .logoutSuccessUrl("/") // 로그아웃 후 리다이렉팅 될 URL
+            .and()
+                // OAuth2 로그인 관련 설정
                 .oauth2Login()
-                .userInfoEndpoint()
-                .userService(customOAuth2UserService);
+                .userInfoEndpoint() // 로그인 성공 시, 사용자 정보 설정
+                .userService(customOAuth2UserService); // 로그인 성공 후, 사용자를 다룰 서비스 등록
     }
 }
