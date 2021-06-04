979912bead645ba2c31de9d45e7c6d0ffcfdf70d
YoungJun Park <youngjun108059@gmail.com>
2021-06-04 23:25:43 +0900

    [#6] 라벨-사진 관계 Domain 연관관계 매핑

--- src/main/java/fullstack/labelary/domain/Relation.java
+++ src/main/java/fullstack/labelary/domain/Relation.java
@@ -1,59 +1,54 @@
 package fullstack.labelary.domain;
 
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-@Getter @Setter
-public class Relation {
+public class Relation extends BaseTimeEntity {
 
     @Id @GeneratedValue
     @Column(name = "relation_idx")
     private Long relationIdx;   // 관계 번호
-    private LocalDateTime relationDt;  // 관계 생성 시간
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "mem_idx")
     private Member member = new Member();
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "picture_idx")
-    private Picture picture = new Picture();
-
-    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "label_idx")
     private Label label = new Label();
-//
-//    //==연관관계 편의 메서드==//
-//    public void setMember(Member member) {
-//        this.member = member;
-//        member.getRelation().add(this);
-//    }
-//
-//    //==연관관계 편의 메서드==//
-//    public void setPicture(Picture picture) {
-//        this.picture = picture;
-//        picture.getRelation().add(this);
-//    }
-//
-//    //==연관관계 편의 메서드==//
-//    public void setLabel(Label label) {
-//        this.label = label;
-//        label.getRelation().add(this);
-//    }
 
-    //==생성 메서드==//
-    public static Relation createRelation (Member member, Picture picture, Label label) {
-        Relation relation = new Relation();
-        relation.setMember(member);
-        relation.setPicture(picture);
-        relation.setLabel(label);
+    private String picturePath;
 
-        relation.setRelationDt(LocalDateTime.now());
-
-        return relation;
+    /**
+     * 연관관계 편의 메서드
+     * @param member 회원 Entity
+     */
+    public void setMember(Member member) {
+        this.member = member;
+        member.getRelations().add(this);
     }
+
+    /**
+     * 연관관계 편의 메서드
+     * @param label 라벨 Entity
+     */
+    public void setLabel(Label label) {
+        this.label = label;
+        label.getRelations().add(this);
+    }
+
+    @Builder
+    public Relation(Member member, Label label, String picturePath) {
+        this.member = member;
+        this.label = label;
+        this.picturePath = picturePath;
+    }
+
 }
