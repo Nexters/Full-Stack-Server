903796bde4054560dfa308427cfeb48bf79fdde4
YoungJun Park <youngjun108059@gmail.com>
2021-06-10 23:48:13 +0900

    [Fix] 라벨 생성 요청 DTO 수정

--- src/main/java/fullstack/labelary/dto/label/LabelSaveRequestDto.java
+++ src/main/java/fullstack/labelary/dto/label/LabelSaveRequestDto.java
@@ -20,12 +20,15 @@ public class LabelSaveRequestDto {
     @NotEmpty
     private String labelColor;      // 라벨 색
     private String labelDetail;     // 라벨 설명
+    @NotEmpty
+    private long memIdx;
 
     @Builder
-    public LabelSaveRequestDto(String labelTitle, String labelColor, String labelDetail) {
+    public LabelSaveRequestDto(String labelTitle, String labelColor, String labelDetail, long memIdx) {
         this.labelTitle = labelTitle;
         this.labelColor = labelColor;
         this.labelDetail = labelDetail;
+        this.memIdx = memIdx;
     }
 
     public Label toEntity() {
