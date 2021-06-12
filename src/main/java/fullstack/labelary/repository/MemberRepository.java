d73a9f65a4fb78da37777ac811676c2cb90e6177
YoungJun Park <youngjun108059@gmail.com>
2021-06-13 00:49:13 +0900

    [#6] 회원 중복 검사 로직

--- src/main/java/fullstack/labelary/repository/MemberRepository.java
+++ src/main/java/fullstack/labelary/repository/MemberRepository.java
@@ -6,5 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    /**
+     * 이미 등록된 email 인지 검사
+     * @param email 이메일
+     * @return 일치하는 회원 정보
+     */
     Optional<Member> findByMemEmail(String email);
 }
