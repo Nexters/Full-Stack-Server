5857b7218d3cbedd8166ab9b47d9fd98c823e312
YoungJun Park <youngjun108059@gmail.com>
2021-06-26 23:31:28 +0900

    [Fix] Label DTO 수정

--- src/main/java/fullstack/labelary/dto/label/LabelGetResponseDto.java
+++ src/main/java/fullstack/labelary/dto/label/LabelGetResponseDto.java
@@ -1,28 +1,28 @@
-package fullstack.labelary.dto.label;
-
-import fullstack.labelary.domain.Label;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-
-/**
- * 라벨 데이터 Get Response
- */
-@Getter
-@NoArgsConstructor
-public class LabelGetResponseDto {
-    private Long labelIdx;
-    private String labelTitle;
-    private String labelDetail;
-    private String labelColor;
-
-    @Builder
-    public LabelGetResponseDto(Label entity) {
-        this.labelIdx = entity.getLabelIdx();
-        this.labelTitle = entity.getLabelTitle();
-        this.labelDetail = entity.getLabelDetail();
-        this.labelColor = entity.getLabelColor();
-    }
-}
-
+//package fullstack.labelary.dto.label;
+//
+//import fullstack.labelary.domain.Label;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//
+///**
+// * 라벨 데이터 Get Response
+// */
+//@Getter
+//@NoArgsConstructor
+//public class LabelGetResponseDto {
+//    private Long labelIdx;
+//    private String labelTitle;
+//    private String labelDetail;
+//    private String labelColor;
+//
+//    @Builder
+//    public LabelGetResponseDto(Label entity) {
+//        this.labelIdx = entity.getLabelIdx();
+//        this.labelTitle = entity.getLabelTitle();
+//        this.labelDetail = entity.getLabelDetail();
+//        this.labelColor = entity.getLabelColor();
+//    }
+//}
+//
--- src/main/java/fullstack/labelary/repository/LabelRepository.java
+++ src/main/java/fullstack/labelary/repository/LabelRepository.java
@@ -1,7 +1,8 @@
-package fullstack.labelary.repository;
-
-import fullstack.labelary.domain.Label;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LabelRepository extends JpaRepository<Label, Long> {
-}
+//package fullstack.labelary.repository;
+//
+//import fullstack.labelary.domain.Label;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+//
+//public interface LabelRepository extends JpaRepository<Label, Long>, JpaSpecificationExecutor<Label> {
+//}
