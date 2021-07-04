5e2bdd706c404e34297ff67aa78af388c78f1907
YoungJun Park <youngjun108059@gmail.com>
2021-07-04 23:47:33 +0900

    [Fix] 사진, 회원 웹 기능 제거

---
+++ src/main/java/fullstack/labelary/controller/MemberApiController.java
@@ -0,0 +1,30 @@
+//package fullstack.labelary.controller;
+//
+//import fullstack.labelary.config.auth.dto.SessionUser;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import javax.servlet.http.HttpSession;
+//
+//@RequiredArgsConstructor
+//@RestController
+//public class MemberApiController {
+////
+////    private final HttpSession httpSession;
+////
+////    @GetMapping("/")
+////    public Object testData() {
+////        return (SessionUser) httpSession.getAttribute("user");
+//    }
+////
+////    @GetMapping("/")
+////    public Object googleLogin() {
+////        return (SessionUser) httpSession.getAttribute("user");
+////    }
+//
+//
+//
+//
+//}
+//
---
+++ src/main/java/fullstack/labelary/repository/PictureRepository.java
@@ -0,0 +1,7 @@
+//package fullstack.labelary.repository;
+//
+//import fullstack.labelary.domain.Picture;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface PictureRepository extends JpaRepository<Picture, Long> {
+//}
