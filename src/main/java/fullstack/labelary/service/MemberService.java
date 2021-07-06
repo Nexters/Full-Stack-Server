0722ee1555622054f4fd4837bd3ee20813841aa1
YoungJun Park <youngjun108059@gmail.com>
2021-07-06 22:49:45 +0900

    [Fix] 회원 비즈니스 로직 제거 (테스트)

--- src/main/java/fullstack/labelary/service/MemberService.java
+++ src/main/java/fullstack/labelary/service/MemberService.java
@@ -1,22 +1,22 @@
-package fullstack.labelary.service;
-
-import fullstack.labelary.repository.MemberRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@RequiredArgsConstructor
-@Service
-public class MemberService {
-
-    private final MemberRepository memberRepository;
-
-    /**
-     * 회원이 존재하는지 검사
-     * @param memIdx 회원 번호
-     * @return 존재 여부
-     */
-    public boolean exist(Long memIdx) {
-        return memberRepository.existsById(memIdx);
-    }
-
-}
+//package fullstack.labelary.service;
+//
+//import fullstack.labelary.repository.MemberRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//@RequiredArgsConstructor
+//@Service
+//public class MemberService {
+//
+//    private final MemberRepository memberRepository;
+//
+//    /**
+//     * 회원이 존재하는지 검사
+//     * @param memIdx 회원 번호
+//     * @return 존재 여부
+//     */
+//    public boolean exist(Long memIdx) {
+//        return memberRepository.existsById(memIdx);
+//    }
+//
+//}
