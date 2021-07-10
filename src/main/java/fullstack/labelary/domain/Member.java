a0cd37115edf977b1ed710cbfa0d1513730a1406
YoungJun Park <youngjun108059@gmail.com>
2021-07-10 23:52:15 +0900

    [Feature] OAuth 적용을 위한 회원 도메인 수정

--- src/main/java/fullstack/labelary/domain/Member.java
+++ src/main/java/fullstack/labelary/domain/Member.java
@@ -3,10 +3,9 @@ package fullstack.labelary.domain;
 import lombok.*;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
+@Table(name = "member")
 @ToString(of = {"memIdx", "memName", "memEmail"})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
@@ -14,43 +13,49 @@ public class Member extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "mem_idx")
     private Long memIdx; // 회원 idx
 
-    @Column(nullable = false)
+    @Column(name = "mem_email", nullable = false)
     private String memEmail; // 이메일
 
-    @Column(nullable = false)
+    @Column(name = "mem_name", nullable = false)
     private String memName; // 회원 이름
 
-    @Column(nullable = false)
+    @Column(name = "mem_picture")
     private String picture; // 회원 프로필사진
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(name = "role", nullable = false)
     private Role role; // 스프링 시큐리티를 위한 권한 코드
 
-    @OneToMany(mappedBy = "member")
-    @ToString.Exclude
-    private List<Label> labels = new ArrayList<>();
+    @Enumerated(EnumType.STRING)
+    @Column(name = "provider", nullable = false)
+    private Provider provider; // OAuth2 Provider
 
-    public void setLabels(List<Label> labels) {
-        this.labels = labels;
-    }
 
-    @OneToMany(mappedBy = "member")
-    @ToString.Exclude
-    private List<Relation> relations = new ArrayList<>();
+//    @OneToMany(mappedBy = "member")
+//    @ToString.Exclude
+//    private List<Label> labels = new ArrayList<>();
+//    public void setLabels(List<Label> labels) {
+//        this.labels = labels;
+//    }
+//
+//    @OneToMany(mappedBy = "member")
+//    @ToString.Exclude
+//    private List<Relation> relations = new ArrayList<>();
 
-    public void setRelations(List<Relation> relations) {
-        this.relations = relations;
-    }
+//    public void setRelations(List<Relation> relations) {
+//        this.relations = relations;
+//    }
 
     @Builder
-    public Member(String memEmail, String memName, String picture, Role role) {
+    public Member(String memEmail, String memName, String picture, Role role, Provider provider) {
         this.memEmail = memEmail;
         this.memName = memName;
         this.picture = picture;
         this.role = role;
+        this.provider = provider;
     }
 
     public Member update(String memName, String picture) {
