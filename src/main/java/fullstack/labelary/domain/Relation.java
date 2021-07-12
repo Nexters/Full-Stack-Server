4f9860280890c043674f072346b3c0584b6dc71a
YoungJun Park <youngjun108059@gmail.com>
2021-07-12 22:13:30 +0900

    [Fix] 회원 - 라벨링 연관관계 제거 테스트

--- src/main/java/fullstack/labelary/domain/Relation.java
+++ src/main/java/fullstack/labelary/domain/Relation.java
@@ -1,54 +1,56 @@
-package fullstack.labelary.domain;
-
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Entity
-public class Relation extends BaseTimeEntity {
-
-    @Id @GeneratedValue
-    @Column(name = "relation_idx")
-    private Long relationIdx;   // 관계 번호
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "mem_idx")
-    private Member member = new Member();
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "label_idx")
-    private Label label = new Label();
-
-    private String picturePath;
-
-    /**
-     * 연관관계 편의 메서드
-     * @param member 회원 Entity
-     */
-    public void setMember(Member member) {
-        this.member = member;
-        member.getRelations().add(this);
-    }
-
-    /**
-     * 연관관계 편의 메서드
-     * @param label 라벨 Entity
-     */
-    public void setLabel(Label label) {
-        this.label = label;
-        label.getRelations().add(this);
-    }
-
-    @Builder
-    public Relation(Member member, Label label, String picturePath) {
-        this.member = member;
-        this.label = label;
-        this.picturePath = picturePath;
-    }
-
-}
+//package fullstack.labelary.domain;
+//
+//import lombok.AccessLevel;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//import javax.persistence.*;
+//
+//@Getter
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//@Entity
+//public class Relation extends BaseTimeEntity {
+//
+//    @Id @GeneratedValue
+//    @Column(name = "relation_idx")
+//    private Long relationIdx;   // 관계 번호
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "mem_idx")
+//    private Member member = new Member();
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "label_idx")
+//    private Label label = new Label();
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "picture_idx")
+//    private Picture picture = new Picture();
+//
+//    /**
+//     * 연관관계 편의 메서드
+//     * @param member 회원 Entity
+//     */
+//    public void setMember(Member member) {
+//        this.member = member;
+//        member.getRelations().add(this);
+//    }
+//
+//    /**
+//     * 연관관계 편의 메서드
+//     * @param label 라벨 Entity
+//     */
+//    public void setLabel(Label label) {
+//        this.label = label;
+//        label.getRelations().add(this);
+//    }
+//
+//    @Builder
+//    public Relation(Member member, Label label, Picture picture) {
+//        this.member = member;
+//        this.label = label;
+//        this.picture = picture;
+//    }
+//
+//}
