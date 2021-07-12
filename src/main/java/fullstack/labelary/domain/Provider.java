e151ff1d02871de263fb2c576bcdc312b1844bad
YoungJun Park <youngjun108059@gmail.com>
2021-07-12 22:14:11 +0900

    [Feature] 프로바이더 도메인 추가(OAuth)

---
+++ src/main/java/fullstack/labelary/domain/Provider.java
@@ -0,0 +1,10 @@
+package fullstack.labelary.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Provider {
+    GOOGLE, KAKAO, NAVER, FACEBOOK
+}
