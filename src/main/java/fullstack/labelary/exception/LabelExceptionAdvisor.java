63e973a9d263e74dd6380292c099db28b14420d3
YoungJun Park <youngjun108059@gmail.com>
2021-07-16 23:31:13 +0900

    [Feature] 라벨 ExceptionAdvisor 테스트

---
+++ src/main/java/fullstack/labelary/exception/LabelExceptionAdvisor.java
@@ -0,0 +1,31 @@
+package fullstack.labelary.exception;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@ControllerAdvice
+@RestController
+public class LabelExceptionAdvisor {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public String processValidationError(MethodArgumentNotValidException exception) {
+        BindingResult bindingResult = exception.getBindingResult();
+
+        StringBuilder builder = new StringBuilder();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            builder.append("[");
+            builder.append(fieldError.getField());
+            builder.append("](은)는 ");
+            builder.append(fieldError.getDefaultMessage());
+            builder.append(" 입력된 값: [");
+            builder.append(fieldError.getRejectedValue());
+            builder.append("]");
+        }
+
+        return builder.toString();
+    }
+}
