bc01db617caf59d278ad3980974d178f8c60d1da
YoungJun Park <youngjun108059@gmail.com>
2021-07-17 01:02:03 +0900

    [Refactor] 미사용 코드 제거

--- src/main/java/fullstack/labelary/repository/MemberRepository.java
+++ src/main/java/fullstack/labelary/repository/MemberRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
     /**
      * 이미 등록된 email 인지 검사
+     *
      * @param email 이메일
      * @return 일치하는 회원 정보
      */
